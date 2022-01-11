78f039e1762464b10fe3fdbddb783fc0974bb886
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-01-11 10:28:05 +0200

    Create src folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,31 +4,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 /* eslint-enable */
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    devServer: {
+        static: './dist',
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
     ],
-  },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
